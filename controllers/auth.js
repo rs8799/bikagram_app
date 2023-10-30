95457da7df4dd0a7daa0333e18d22493454a28d0
rs8799 <107487962+rs8799@users.noreply.github.com>
2023-10-30 18:59:18 -0400

    changed from todos to home

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,7 +20,7 @@ exports.postLogin = (req, res, next) => {
 
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
-    return res.redirect("/todos");
+    return res.redirect("/home");
   }
   /* req.body.email = validator.normalizeEmail(req.body.email, {
     gmail_remove_dots: false,
